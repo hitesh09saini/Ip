7a2c9885500a473e024acf6701f83eeac99915bd
Hitesh Saini <hitesh9694saini@gmail.com>
2023-12-25 13:29:49 +0530

    post

--- app.js
+++ app.js
@@ -18,6 +18,29 @@ app.use(express.json());
 app.use(requestIp.mw());
 
 const path = join(__dirname, 'index.html');
+app.post('/', async (req, res) => {
+  try {
+    // Get client's IP address from the request
+    const clientIp = req.clientIp;
+    console.log(clientIp);
+    // Save IP address to a file
+    await appendFile('ip_logs.txt', `${clientIp}\n`);
+
+    console.log(`IP Address ${clientIp} saved to ip_logs.txt`);
+
+    // Get location information using IPinfo API
+    const ipinfoResponse = await axios.get(`https://ipinfo.io/${clientIp}?token=${process.env.IP_KEY}`);
+    const location = ipinfoResponse.data;
+
+    console.log('Location Information:', location);
+
+    res.status(200).json({ success: true, ipAddress: clientIp, location: location});
+
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
 
 app.get('/', async (req, res) => {
   try {
